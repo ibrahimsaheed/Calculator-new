9e46796f258831fb3cda52f0b2b77f1cae0fef38
Saheed Ibrahim <ibrahim.saheed2005@gmail.com>
2023-06-13 13:28:06 +0100

    addCreate a function to add two numbers together

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function add(num1,num2) {
+    return num1+num2;
+}
+const number1 =parseInt(prompt("Enter the first number"))
+const number2 =parseInt(prompt("Enter the second number"))
+const result="The answer is "+ add(number1,number2)
+alert(result)
